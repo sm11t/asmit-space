050860a9ce8ccd99073e1f04a58394bb74b52257
sm11t <adatta18@asu.edu>
2026-02-10 14:08:52 -0700

    Update API URL: https://newest-forge-butler-russia.trycloudflare.com

---
+++ pash/config.js
@@ -0,0 +1,5 @@
+// Runtime configuration
+// Edit this file on Hostinger to change API URL without rebuilding
+window.PASH_CONFIG = {
+  API_URL: 'https://stickers-kenneth-otherwise-addresses.trycloudflare.com'
+};
