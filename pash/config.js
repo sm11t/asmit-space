46476beb53481cc028c50cd052d16ff94c4695e0
sm11t <adatta18@asu.edu>
2026-02-13 14:09:46 -0700

    Deploy: 2026-02-13 14:09

--- pash/config.js
+++ pash/config.js
@@ -1,5 +1,6 @@
 // Runtime configuration - Edit this file on Hostinger to change API URL
 // No rebuild needed!
 window.PASH_CONFIG = {
-  API_URL: 'https://pash-api.asmit.space'
+  API_URL: 'https://pash-api.asmit.space',
+  GOOGLE_CLIENT_ID: '274823270860-7j01ac6bfeirb2pcv6ssdq2gt39uaeqe.apps.googleusercontent.com'
 };
